781f3b1a290a73a0092ba1363a38c2fd5794db45
Coddo-Python <coddobusiness@gmail.com>
2022-12-20 16:50:06 +0400

    First upload of landing page

---
+++ components/Planet.vue
@@ -0,0 +1,23 @@
+<template>
+  <img class="hidden lg:block absolute z-10" alt="Planet">
+</template>
+
+<script>
+export default {
+  name: "Planet",
+  methods: {
+    onMouseMove(e) {
+      const x = e.clientX / window.innerWidth
+      const y = e.clientY / window.innerHeight
+      this.$el.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)'
+    }
+  },
+  mounted () {
+    window.addEventListener('mousemove', this.onMouseMove)
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Star.vue
@@ -0,0 +1,29 @@
+<template>
+  <img src="/images/Star.svg" class="w-2 absolute top-0 left-0" :style="{'top': top, 'left': left}" alt="Star">
+</template>
+
+<script>
+export default {
+  name: "Star",
+  data () {
+    return {
+      top: Math.random() * 100 + 'vh',
+      left: Math.random() * 100 + 'vw',
+    }
+  },
+  methods: {
+    onMouseMove(e) {
+      const x = e.clientX / window.innerWidth
+      const y = e.clientY / window.innerHeight
+      this.$el.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'
+    }
+  },
+  mounted () {
+    window.addEventListener('mousemove', this.onMouseMove)
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
